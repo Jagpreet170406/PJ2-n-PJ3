ec6262336daab5dfb4da987248f749fb1ff43234
Jayden <251682F@mymail.nyp.edu.sg>
2026-01-25 13:59:48 +0800

    Your changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\jagsi\Documents\NYP\Y1S2 MODS\(IT1925) Web Development Project (WDP-WIU)\PJ2 n PJ3\venv"
+set "VIRTUAL_ENV=C:\Users\jdent\OneDrive\Desktop\wdp_A\PJ2-n-PJ3\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
